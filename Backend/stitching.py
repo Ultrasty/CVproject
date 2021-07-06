200208f81c5ff97996bbe3fa5adfd1e7fd232c1c
sty <1455670697@qq.com>
2021-07-07 01:53:47 +0800

    update

--- Backend/stitching.py
+++ Backend/stitching.py
@@ -9,17 +9,17 @@ import cv2
 import matplotlib.pyplot as plt
 
 # construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--first", required=True,
-                help="path to the first image")
-ap.add_argument("-s", "--second", required=True,
-                help="path to the second image")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-f", "--first", required=True,
+#                 help="path to the first image")
+# ap.add_argument("-s", "--second", required=True,
+#                 help="path to the second image")
+# args = vars(ap.parse_args())
 
 # load the two images and resize them to have a width of 400 pixels
 # (for faster processing)
-imageA = cv2.imread(args["first"])
-imageB = cv2.imread(args["second"])
+imageA = cv2.imread("images/bryce_left_01.png")
+imageB = cv2.imread("images/bryce_right_01.png")
 imageA = imutils.resize(imageA, width=400)
 imageB = imutils.resize(imageB, width=400)
 
